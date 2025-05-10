29011b48e94e56daab751f423d768b2a654b12e2
QuangMinh0806 <lequangminh07072003@gmail.com>
2025-05-10 22:54:34 +0700

    update booking

--- Backend/model/booking_details.js
+++ Backend/model/booking_details.js
@@ -1,5 +1,8 @@
 const { Sequelize, DataTypes } = require('sequelize');
 const { sequelize } = require("../config/mysql");
+const { Booking } = require('./booking');
+const { Inventory } = require('./inventory');
+const { Room_Details } = require('./room_details');
 
 const Booking_Details = sequelize.define("Booking_Details",
     {
@@ -10,5 +13,40 @@ const Booking_Details = sequelize.define("Booking_Details",
     }
 );
 
+//Booking
+Booking_Details.belongsTo(Booking);
+Booking.hasMany(Booking_Details);
+
+
+//Room_Details
+Booking_Details.belongsTo(Room_Details);
+Room_Details.hasMany(Booking_Details);
+
+Booking_Details.afterBulkCreate(async (data) => {
+
+    const id = await Booking.findOne({
+        where : {
+            id : data[0].dataValues.BookingId
+        },
+        attributes : ["checkin", "checkout"]
+    })
+
+
+    const sum = (new Date(id.dataValues.checkout) - new Date(id.dataValues.checkin)) /(1000 * 3600 * 24);
+    const checkinDate = new Date(id.dataValues.checkin);
+
+
+    for(const booking of data){
+        for(let i = 0; i<sum; i++){
+            const day = new Date(checkinDate);
+            day.setDate(checkinDate.getDate() + i);
+            await Inventory.create({
+                inventory_date : day,
+                RoomDetailId : booking.dataValues.RoomDetailId
+            })
+        }
+    }
+
+});
 
 module.exports = { Booking_Details };
